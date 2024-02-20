9e804417162650ed18daebb2f6053837fe4c98aa
Vaibhav <direct2vaibhavkore@gmail.com>
2024-02-20 14:39:27 +0530

    app.py changes

--- flaskserver/app.py
+++ flaskserver/app.py
@@ -7,6 +7,14 @@ from rake_nltk import Rake
 
 app = Flask(__name__)
 
+# helper function to lemmatize the text
+def lemmatize_text(text):
+    lemmatizer = WordNetLemmatizer()
+    tokenized = word_tokenize(text)
+    lemmatized = [lemmatizer.lemmatize(word) for word in tokenized]
+    lemmatized_text = ' '.join(lemmatized)
+    return lemmatized_text
+
 @app.route('/')
 def index():
     return 'Hello, World!'
@@ -14,16 +22,14 @@ def index():
 @app.route('/kextract',methods=['POST'])
 def kextract():
     text = request.form['text']
-    lemmatizer = WordNetLemmatizer()
     r = Rake()
     try:
-        # lemmitize the text
-        tokenized = word_tokenize(text)
-        lemmatized = [lemmatizer.lemmatize(word) for word in tokenized]
-        lemmatized_text = ' '.join(lemmatized)
-        print(lemmatized_text)
-        # extract keywords from sentence
-        
+        # lemmatize the text
+        try:
+            lemmatized_text = lemmatize_text(text)
+        except Exception as e:
+            return str(e)
+        print(lemmatized_text)        
         r.extract_keywords_from_text(lemmatized_text)
         keywords = r.get_ranked_phrases_with_scores()
         # return json response with keywords and their scores
