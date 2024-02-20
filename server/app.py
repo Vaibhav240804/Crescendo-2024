3db2693e18ed0887d5351fb3bde1d5f9f613af39
Vaibhav <direct2vaibhavkore@gmail.com>
2024-02-20 13:50:58 +0530

    exploring tf-idf

--- server/app.py
+++ server/app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask import request
+from rake_nltk import Rake
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/kextract',methods=['POST'])
+def kextract():
+    text = request.form['text']
+    r = Rake()
+    try:
+        r.extract_keywords_from_text(text)
+        return {'keywords':r.get_ranked_phrases()}
+    except:
+        return {'keywords':[]}
+    
 
 @app.route('/about')
 def about():
