18fb29095eab0f4deeca0bf30952015ad3002528
Aditya Ningule <adiningule2@gmail.com>
2024-02-21 02:42:39 +0530

    updated sva

--- flaskserver/app.py
+++ flaskserver/app.py
@@ -120,10 +120,6 @@ def extractReviews(rurl, uurl):
 
 
 valid_timeframes = [
-    "now 1-d",
-    "now 1-H",
-    "now 4-H",
-    "now 1-d",
     "now 7-d",
     "today 1-m",
     "today 3-m",
@@ -270,30 +266,24 @@ def start():
 
 @app.route('/sva', methods=['GET'])
 def interest_over_time():
-    timeframe_choice = request.args.get('timeframe_choice', type=int)
-
-    if not 1 <= timeframe_choice <= len(valid_timeframes):
-        return jsonify({'error': 'Invalid timeframe choice. Please enter a valid number.'}), 400
-
-    timeframe = valid_timeframes[timeframe_choice - 1]
-
     pytrends = TrendReq(hl='en-US', tz=360)
-
     product_name = data['name'][0]
     kw_list = [product_name]
     geo = "IN"
+    
+    sva_data = {}
 
-    pytrends.build_payload(kw_list, cat=0, timeframe=timeframe, geo=geo)
-    interest_over_time_df = pytrends.interest_over_time().reset_index()
+    for timeframe in valid_timeframes:
+        pytrends.build_payload(kw_list, cat=0, timeframe=timeframe, geo=geo)
+        interest_over_time_df = pytrends.interest_over_time().reset_index()
+        interest_over_time_df['date'] = interest_over_time_df['date'].astype(str)
+        sva_data[timeframe] = interest_over_time_df.rename(columns={product_name: 'score'})[['date', 'score']].to_dict(orient='records')
 
-    interest_over_time_df['date'] = interest_over_time_df['date'].astype(str)
+    result = {'sva': sva_data}
 
-    result = {
-        'interest_over_time': interest_over_time_df[['date', product_name]].to_dict(orient='records')
-    }
     return jsonify(result), 200
 
-
+# ---------------------------------lda------------------------------------
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 
