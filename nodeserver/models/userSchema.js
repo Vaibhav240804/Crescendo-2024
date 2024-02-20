59cbf1eed50f4044a22bd3f8a1eca8bd4520a3c2
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2024-02-20 19:47:12 +0530

    user product updation done

--- nodeserver/models/userSchema.js
+++ nodeserver/models/userSchema.js
@@ -1,5 +1,96 @@
 import mongoose from 'mongoose';
 
+const reviewSchema = new mongoose.Schema({
+  // title: {
+  //   type: String,
+  //   required: true,
+  // },
+  rating: {
+    type: String,
+    required: true,
+  },
+  star: {
+    type: Number,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  fullDate: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+})
+
+const sentimentSchema = new mongoose.Schema({
+  negative: {
+    type: Number,
+    required: true,
+  },
+  neutral: {
+    type: Number,
+    required: true,
+  },
+  positive: {
+    type: Number,
+    required: true,
+  },
+})
+
+const keywordSchema = new mongoose.Schema({
+  word: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+})
+
+const svaSchema = new mongoose.Schema({
+  date: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+})
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  reviews: [reviewSchema],
+  sentiment: sentimentSchema,
+  keywords: [keywordSchema],
+  sva: [svaSchema]
+});
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -17,7 +108,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-  }
+  },
+  products: [productSchema]
 });
 
 const User = mongoose.model("cres_user", userSchema);
